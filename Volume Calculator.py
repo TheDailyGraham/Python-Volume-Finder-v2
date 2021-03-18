ecbc0ef16986f78cd49169a759e4e7e20a28a827
TheDailyGraham <79054337+TheDailyGraham@users.noreply.github.com>
2021-03-18 15:59:28 -0700

    Add files via upload

---
+++ Volume Calculator.py
@@ -0,0 +1,6 @@
+print("This a volume finder in python")
+W = float(input("Enter the width: "))
+H = float(input("Enter the height: "))
+D = float(input("Enter the depth: "))
+V = W * H * D
+print(V)
